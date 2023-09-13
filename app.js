8cb449fe719bdf8424e3c7a7bb0620cf6f3a146b
Divyansh Singh Pangtey <pangteydivyansh@gmail.com>
2023-09-13 20:45:31 +0530

    updated mongodb uri

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@blogdb.6k4lvnv.mongodb.net/test`,
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@blogdb.6k4lvnv.mongodb.net/test&authSource=admin`,
     {
       useUnifiedTopology: true,
       useNewUrlParser: true,
